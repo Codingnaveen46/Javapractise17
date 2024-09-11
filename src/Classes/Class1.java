ce447fc1215d6e04ac844cad54d1f30860dc8a8b
codingnaveen46 <naveenreddy4411@.com>
2024-09-11 17:40:22 +0530

    Learnt about the classes

---
+++ src/Classes/Class1.java
@@ -0,0 +1,18 @@
+package Classes;
+
+public class Class1 {
+
+                //class is a template / data type that can store methods class,objects
+                String myName = "John";
+                int myAge = 20;
+
+    
+
+
+
+        public static void main(String[] args) {
+
+
+
+    }
+}
