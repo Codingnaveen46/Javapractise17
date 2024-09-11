0bd0c7c3784543e271b300689acee2813035d05d
codingnaveen46 <naveenreddy4411@.com>
2024-09-11 11:06:42 +0530

    Added new comment in the editor

--- src/language/Basics/FirstJavaProgram.java
+++ src/language/Basics/FirstJavaProgram.java
@@ -41,5 +41,6 @@ public class FirstJavaProgram {
         System.out.println("Hello World");
         System.out.print("Hey"); // print in the same line
         //new commit
+        //newcomment
     }
 }
