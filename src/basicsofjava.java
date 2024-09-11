e38e7d0906ba0662412b039447aeec51705a9dac
codingnaveen46 <naveenreddy4411@.com>
2024-09-11 10:15:59 +0530

    learnt basics of java

---
+++ src/basicsofjava.java
@@ -0,0 +1,29 @@
+public class basicsofjava {
+
+    public static void main(String[] args) {
+        //single line comment
+
+        /*
+        multiline comment
+         */
+
+        //what is java ?
+         /* java is a class based object-oriented programming
+         language for building web ,mobile,and desktop applications.
+         Java was developed by sun microsystems inc in 1995 later
+         acquired by oracle corporation.
+          */
+
+        //JDK == Java development kit
+        //JRE == Java runtime environment
+        // JVM == Java virtual machine
+
+
+        //jvm is converted into binary language (010101101)
+        //new
+
+
+
+
+    }
+}
