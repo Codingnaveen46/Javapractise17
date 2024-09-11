476a8e1023d11d3336d05598751bb331a7cf6129
codingnaveen46 <naveenreddy4411@.com>
2024-09-11 17:40:25 +0530

    Learnt about the classes

--- src/Classes/Class1.java
+++ src/Classes/Class1.java
@@ -2,17 +2,15 @@ package Classes;
 
 public class Class1 {
 
-                //class is a template / data type that can store methods class,objects
-                String myName = "John";
-                int myAge = 20;
+     //class is a template / data type that can store methods class,objects.
+    //Object is new instance of class
 
-    
+    //syntax of object is  using new class1() keyword
 
+    String myName = "John";
+    int myAge = 20;
 
-
-        public static void main(String[] args) {
-
-
+    public static void main(String[] args) {
 
     }
 }
---
+++ src/Classes/Class2.java
@@ -0,0 +1,30 @@
+package Classes;
+
+public class Class2 {
+
+    public static void main(String[] args) {
+
+        Class1 Obj = new Class1();
+        Class1 Obj2 = new Class1();
+        Class1 Obj3 = new Class1();
+
+        Obj.myName = "John2";
+        Obj.myAge = 20;
+        System.out.println(Obj.myName);
+
+        Obj2.myName = "John3";
+        Obj2.myAge = 20;
+        System.out.println(Obj2.myName);
+
+        Obj3.myName = "John4";
+        Obj3.myAge = 20;
+        System.out.println(Obj3.myName);
+
+        // we can create as many as object with different name using a class.
+
+
+
+
+
+    }
+}
