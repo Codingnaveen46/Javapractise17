81e6afd92bf367db9ad268306e6db3e5c0fcec8f
codingnaveen46 <naveenreddy4411@.com>
2024-09-17 10:38:06 +0530

    Learnt about the variables and rules

---
+++ src/variables_rulesOFjava/Variableslearning.java
@@ -0,0 +1,45 @@
+package variables_rulesOFjava;
+
+public class Variableslearning {
+
+    public static void main(String[] args) {
+
+        //datatype  variable = "value/ data" ;
+        //what is data is : the nature of data type what type of data is stored
+
+        //Rules to follow while using the variables
+
+        //1 the variable /identifier should not be same as java reserved keyword.
+          //  String int = "naveen";
+
+        //2 the variable/identifier should not be the same as java literal (true/false/null);
+            //String true = bharath;
+
+        //3 the variable/identifier should not contain any white space.
+            //String emp name = "bharath";
+
+        //4 the variable/identifier should not contain any special characters;
+            // String emp@name = "bharaj";
+
+        //5 the variable/identifier should not start with numbers;
+            // Sting 1empname = "BHarath";
+
+        //6 the variables/identifier should be unique;
+            //String empName = "BHarath";
+
+        //7 the variable/identifier is case sensetive;
+            //String EmpName = "Bharath";
+
+        //Standards
+
+            // variables should start with lowercase followed by camelCasing
+                String nameOfEmp = "Naveen";
+            // variables should be meaningful and it should be with in 3 to 15 char length;
+                int carNumber = 44554;
+
+
+
+
+
+    }
+}
