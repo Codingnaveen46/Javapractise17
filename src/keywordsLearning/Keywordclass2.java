530ab860667b78eadd7066ebd8c2bc708d0d5730
codingnaveen46 <naveenreddy4411@.com>
2024-09-24 10:20:44 +0530

    learnt about Keywords in java SuperKeyword, This keyword , enum keyword

---
+++ src/keywordsLearning/Keywordclass2.java
@@ -0,0 +1,14 @@
+package keywordsLearning;
+
+public  class keywordClass2 extends  KeywordClass1{
+
+    String name = "NAveen3";
+    public  void printName(String name){
+        System.out.println(name);
+    }
+
+}
+
+public static void main(String[] args) {
+
+}
