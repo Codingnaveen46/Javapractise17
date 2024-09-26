b5c31c96224c1ada7fcda42120abb5fbbfb62df2
codingnaveen46 <naveenreddy4411@.com>
2024-09-26 09:25:16 +0530

    Added some interview questions

--- src/exceptionHandling/ThrownCustomException.java
+++ src/exceptionHandling/ThrownCustomException.java
@@ -1,5 +1,8 @@
 package exceptionHandling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThrownCustomException {
     public static void main(String[] args) throws Exception {
 
@@ -14,5 +17,39 @@ public class ThrownCustomException {
         //more over we should not encourage the default exceptions like arrayoutofboundindex
         //directly we should not use the throw exception in java instead we can use the Assertions
 
+       /* List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.set(1, "C");
+        list.add("D");
+        System.out.println(list);
+
+
+        try {
+            int a = 10 / 0;
+            System.out.println("This will not be printed");
+        } catch (Exception e) {
+            System.out.println("Exception caught");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+
+        int a = 5;
+        int b = 10;
+        if ((a = 3) == b) {
+            System.out.println(a);
+        } else {
+            System.out.println(a + b);
+        }
+
+        int A = 0;
+        System.out.println(A++ == ++A);
+
+
+        String s1 = new String("xyz");
+        String s2 = "xyz";
+        System.out.println(s1.intern() == s2);
+*/
     }
+
 }
